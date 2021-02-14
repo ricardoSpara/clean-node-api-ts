cd962ef151ae24e927af94f5d53632f4a4cdea15
Ricardo Sparapan <ricardo.jrsparapan@gmail.com>
2021-02-14 19:49:09 -0300

    test: ensure signup controller returns an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,6 +12,8 @@ describe('SingUp Controller', () => {
     };
 
     const httpResponse = sut.handle(httpRequest);
+
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
 });
