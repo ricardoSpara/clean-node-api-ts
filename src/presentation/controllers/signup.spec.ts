06a8b1f604d3622118788436b492d01b56a827d4
Ricardo Sparapan <ricardo.jrsparapan@gmail.com>
2021-02-14 21:17:47 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import SignUpController from './signup';
+import MissingParamError from '../errors/missing-param-error';
 
 describe('SingUp Controller', () => {
   test('Should return 400 id no name is provided', () => {
@@ -14,7 +15,7 @@ describe('SingUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
 
   test('Should return 400 id no email is provided', () => {
@@ -30,6 +31,6 @@ describe('SingUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 });
